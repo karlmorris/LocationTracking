ad5c5f294e1d2dbeaae3e622903588cd0ee885eb
Karl Morris <karl.morris@temple.edu>
2022-01-25 10:51:25 -0500

    Added mapping

--- app/src/main/java/edu/temple/locationtracking/MainActivity.kt
+++ app/src/main/java/edu/temple/locationtracking/MainActivity.kt
@@ -9,8 +9,13 @@ import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     val locationManager : LocationManager by lazy {
         getSystemService(LocationManager::class.java)
@@ -21,12 +26,20 @@ class MainActivity : AppCompatActivity() {
     var previousLocation : Location? = null
     var distanceTraveled = 0f
 
+    lateinit var mapView : MapView
+
+    lateinit var googleMap : GoogleMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.textView)
+        mapView = findViewById(R.id.mapView)
+
+        mapView.getMapAsync(this)
+        mapView.onCreate(savedInstanceState)
 
         if (!permissionGranted()) {
             requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 123)
@@ -36,6 +49,10 @@ class MainActivity : AppCompatActivity() {
             if (previousLocation != null) {
                 distanceTraveled += it.distanceTo(previousLocation)
                 textView.text = distanceTraveled.toString()
+
+                val latLng = LatLng(it.latitude, it.longitude)
+
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16f))
             }
             previousLocation = it
         }
@@ -52,16 +69,37 @@ class MainActivity : AppCompatActivity() {
         return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
 
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
     override fun onResume() {
         super.onResume()
         doGPSStuff()
+        mapView.onResume()
     }
 
     override fun onPause() {
         super.onPause()
         locationManager.removeUpdates(locationListener)
+        mapView.onPause()
     }
 
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -75,5 +113,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        googleMap = p0
     }
 }
